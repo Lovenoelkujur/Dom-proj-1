541d6c6d21162d2676631499c8f4c2a6ba9be020
Noel Kujur <lovenoelkujur@gmail.com>
2024-01-12 18:46:07 +0530

    Initial file

---
+++ script.js
@@ -0,0 +1,19 @@
+const myContainer = document.getElementById("container");
+
+myContainer.style.backgroundColor = "yellow";
+
+myContainer.style.margin = "20px";
+
+myContainer.style.padding = "10px";
+
+myContainer.style.fontSize = "18px";
+
+myContainer.style.fontWeight = "bold";
+
+myContainer.style.height = "200px";
+
+myContainer.style.width = "300px";
+
+//Extra
+
+myContainer.style.color = "darkblue";
